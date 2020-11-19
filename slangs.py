a83e949a5f343b6f7f0e772604190b7982b67ff7
Diamond Acharya <dacharya@princeton.edu>
2020-11-19 11:32:59 -0500

    Add list of slangs to track
    Decided to focus the project on analysing american slang usage in
    nepal as an indicator for westernization

---
+++ slangs.py
@@ -0,0 +1,13 @@
+slangs = {
+    '60s': ['groovy', 'hippie', 'chrome dome', 'bummer', 'bogart', 'flower power', 'marvy',
+            'sock it to me', 'can you dig it?', 'ya dig?', 'you dig?', 'spacey'],
+    '70s': ['truckin', "truckin'", 'retro', 'watergate', 'psyche!', 'boogie', 'brick house',
+            'ditz', 'catch you on the flip side'],
+    '80s': ['chill out', 'gnarly', 'rad', 'wastoid', 'tubular', 'cool beans', "what's your damage?",
+            'eat my shorts'],
+    '90s': ['geek out', 'bestie', 'phat', 'talk to the hand', 'alrighty then',
+            'all that and a bag of chips', 'yadda yadda', 'yada yada', 'dude', 'whassup'],
+    '00s': ['pwned', 'noob', 'chillax', 'awesome sauce', 'life hack', 'totes', 'nom nom',
+            'mansplain', 'subtweet'],
+    '10s': ['yolo', 'bingeable', 'dumpster fire', 'stan']
+}
